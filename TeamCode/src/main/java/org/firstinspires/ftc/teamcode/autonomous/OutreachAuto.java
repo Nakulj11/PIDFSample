a2e035d85614a54521debf6996fdb5b114aeea0d
Nakulj11 <nakulsaliljoshi@gmail.com>
2022-06-30 09:48:17 -0400

    Adjusted constants again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/OutreachAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/OutreachAuto.java
@@ -27,8 +27,8 @@ public class OutreachAuto extends LinearOpMode {
         IDLE
     }
 
-    final double KP = 0.001;
-    final double KI = 0.0001;
+    final double KP = 0.004;
+    final double KI = 0.0004;
     double integralSum=0;
 
     State currentState = State.IDLE;
