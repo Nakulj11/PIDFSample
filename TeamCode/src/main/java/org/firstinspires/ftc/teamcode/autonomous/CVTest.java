8ef0a0e4f88a8253943d921b08eac086eae2cab5
Nakulj11 <nakulsaliljoshi@gmail.com>
2022-09-11 09:37:52 -0400

    added some vision stuff to test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CVTest.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.component.ImageDetectionPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class CVTest extends LinearOpMode {
+    static final int STREAM_WIDTH = 1280; // modify for your camera
+    static final int STREAM_HEIGHT = 720; // modify for your camera
+    OpenCvWebcam webcam;
+    ImageDetectionPipeline pipeline;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = null;
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam"); // put your camera's name here
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        pipeline = new ImageDetectionPipeline(telemetry);
+        webcam.setPipeline(pipeline);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(STREAM_WIDTH, STREAM_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Failed","");
+                telemetry.update();
+            }
+        });
+
+        while (!isStarted() && !isStopRequested()) {
+            telemetry.addData("Detected Location", pipeline.getLocation());
+            telemetry.update();
+        }
+
+
+
+
+        webcam.stopStreaming();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/component/ImageDetectionPipeline.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode.component;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class ImageDetectionPipeline extends OpenCvPipeline {
+    Telemetry telemetry;
+    Mat mat = new Mat();
+
+    //locations
+    public enum Location {
+        ONE,
+        TWO,
+        THREE
+    }
+
+    double[] values = new double[3];
+
+    //location
+    private Location location = Location.ONE;
+
+    //Region of interest coordinates
+    static final Rect DETECTION_ROI = new Rect(
+            new Point(5, 80),
+            new Point(85, 150));
+
+
+    //Threshold to determine if there is an object
+    final static double PERCENT_COLOR_THRESHOLD = 0.1;
+
+    public ImageDetectionPipeline(Telemetry t) { telemetry = t; }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+
+        Scalar[] lowHSV = {new Scalar(12, 180, 130), //ONE
+                new Scalar(12, 180, 130),//TWO
+                new Scalar(12, 180, 130)};//THREE
+
+
+        Scalar[] highHSV = {new Scalar(26, 255, 255),//ONE
+                new Scalar(12, 180, 130),//TWO
+                new Scalar(12, 180, 130)};//THREE
+
+        Mat roi = null;
+        //creats submatrices
+        for(int i=0;i<3;i++){
+            Core.inRange(mat, lowHSV[i], highHSV[i], mat);
+            roi = mat.submat(DETECTION_ROI);
+            //finds the raw value that fits in HSV range
+            values[i] = Core.sumElems(roi).val[0] / DETECTION_ROI.area() / 255;
+        }
+
+
+        roi.release();
+
+
+        int highestIndex = 0;
+        for(int i=1;i<3;i++){
+            if(values[i]>values[highestIndex]){
+                highestIndex = i;
+            }
+        }
+
+        switch(highestIndex){
+            case 0:
+                location = Location.ONE;
+                break;
+            case 1:
+                location = Location.TWO;
+                break;
+            case 2:
+                location = Location.THREE;
+                break;
+            default:
+                break;
+        }
+
+        //stuff to make the gray scale appear on on robot phone
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB);
+
+        Scalar colorRectangle = new Scalar(0, 255, 0);
+
+        Imgproc.rectangle(mat, DETECTION_ROI, colorRectangle);
+
+        return mat;
+    }
+
+    //some methods to get constants and vars
+    public Location getLocation() {
+        return location;
+    }
+
+}
+
+
