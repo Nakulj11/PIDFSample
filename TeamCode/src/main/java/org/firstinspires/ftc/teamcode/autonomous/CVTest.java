04de9edff6af05a9410d09d26f1fac48995dd0c0
Nakulj11 <nakulsaliljoshi@gmail.com>
2022-09-12 06:03:38 -0400

    reorganized some vision stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CVTest.java
@@ -1,41 +1,16 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.component.AprilTagDetectionPipeline;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvWebcam;
+import org.firstinspires.ftc.teamcode.component.Webcam;
 import org.openftc.apriltag.AprilTagDetection;
 
-import java.util.ArrayList;
 
 public class CVTest extends LinearOpMode {
-    static final int STREAM_WIDTH = 1280; // modify for your camera
-    static final int STREAM_HEIGHT = 720; // modify for your camera
-    OpenCvWebcam webcam;
 
-    AprilTagDetectionPipeline aprilTagDetectionPipeline;
-
+    Webcam.Location location = null;
+    AprilTagDetection tagOfInterest = null;
     static final double FEET_PER_METER = 3.28084;
 
-    // Lens intrinsics
-    // UNITS ARE PIXELS
-    // NOTE: this calibration is for the C920 webcam at 800x448.
-    // You will need to do your own calibration for other configurations!
-    double fx = 822.317;
-    double fy = 822.317f;
-    double cx = 319.495;
-    double cy = 242.502;
-
-    // UNITS ARE METERS
-    double tagsize = 0.166;
-
-    AprilTagDetection tagOfInterest = null;
-
-    Location location = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -45,49 +20,15 @@ public class CVTest extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        WebcamName webcamName = null;
-        webcamName = hardwareMap.get(WebcamName.class, "Webcam"); // put your camera's name here
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+        // put this code in robot class later
+        Webcam webcam = new Webcam();
+        webcam.init(hardwareMap);
 
-        webcam.setPipeline(aprilTagDetectionPipeline);
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                webcam.startStreaming(STREAM_WIDTH, STREAM_HEIGHT, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-                telemetry.addData("Camera Failed", "");
-
-                telemetry.update();
-            }
-        });
 
         while (!isStarted() && !isStopRequested()) {
-            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
-
-            if (currentDetections.size() != 0) {
-
-                for (AprilTagDetection tag : currentDetections) {
-                    if (tag.id == Location.ONE.getId()) {
-                        location = Location.ONE;
-                        tagOfInterest = tag;
-                        break;
-                    } else if (tag.id == Location.TWO.getId()) {
-                        location = Location.TWO;
-                        tagOfInterest = tag;
-                        break;
-                    } else if (tag.id == Location.THREE.getId()) {
-                        location = Location.THREE;
-                        tagOfInterest = tag;
-                        break;
-                    }
-                }
-
-            }
+            webcam.scanForTags();
+            location = webcam.getLocation();
+            tagOfInterest = webcam.getTagOfInterest();
 
 
             if (location != null && tagOfInterest != null){
@@ -124,19 +65,4 @@ public class CVTest extends LinearOpMode {
         telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
     }
 
-    public enum Location {
-        ONE (0),
-        TWO (1),
-        THREE (2);
-
-        int id;
-
-        Location(int id){
-            this.id = id;
-        }
-
-        int getId(){
-            return id;
-        }
-    }
 }
